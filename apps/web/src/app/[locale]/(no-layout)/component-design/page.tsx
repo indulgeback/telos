8a2b58bc62b9954b93567440b1a7dfdd2bb59b68
LeviLiu <liuwenyu1937@outlook.com>
2025-07-25 11:41:58 +0800

    feat: rewrite toggle

--- apps/web/src/app/[locale]/(no-layout)/component-design/page.tsx
+++ apps/web/src/app/[locale]/(no-layout)/component-design/page.tsx
@@ -6,22 +6,14 @@ import {
   Textarea,
   Checkbox,
   Badge,
-  Card,
-  Carousel,
-  Drawer,
-  Pagination,
-  Popover,
-  Progress,
-  Select,
-  Slider,
-  Switch,
-  Table,
-  Tabs,
-  Tooltip,
+  Label,
+  Toggle,
+  ToggleGroup,
+  ToggleGroupItem,
 } from '@/components/atoms'
 import { ThemeToggle } from '@/components/molecules'
 import NotFound from '@/app/not-found'
-import { GitBranch } from 'lucide-react'
+import { Bold, GitBranch, Italic, Underline } from 'lucide-react'
 
 const ComponentDesignPage = () => {
   const env = process.env.NEXT_PUBLIC_NODE_ENV
@@ -31,7 +23,7 @@ const ComponentDesignPage = () => {
   ) : (
     <div className='flex flex-col h-screen overflow-auto gap-4 p-4 bg-white dark:bg-black'>
       <ThemeToggle />
-      <div className='gap-4 hidden'>
+      <div className='gap-4 flex'>
         <Button>Button</Button>
         <Button variant='destructive'>Button</Button>
         <Button variant='outline'>Button</Button>
@@ -74,6 +66,62 @@ const ComponentDesignPage = () => {
         <Textarea className='w-[350px]' />
         <Textarea className='w-[350px]' placeholder='Placeholder' />
         <Textarea className='w-[350px]' disabled />
+      </div>
+      <div className='flex flex-col gap-4'>
+        <div className='flex items-center gap-2'>
+          <Checkbox id='checkbox' />
+          <Label htmlFor='checkbox'>Label</Label>
+        </div>
+        <div className='flex items-center gap-2'>
+          <Checkbox id='checkbox' disabled />
+          <Label htmlFor='checkbox'>Label</Label>
+        </div>
+      </div>
+      <div className='flex flex-col items-center gap-4'>
+        <div className='flex w-full flex-wrap gap-4'>
+          <Badge>Badge</Badge>
+          <Badge variant='secondary'>Secondary</Badge>
+          <Badge variant='destructive'>Destructive</Badge>
+          <Badge variant='outline'>Outline</Badge>
+        </div>
+        <div className='flex w-full flex-wrap gap-4'>
+          <Badge
+            variant='secondary'
+            className='bg-blue-500 text-white dark:bg-blue-600'
+          >
+            <GitBranch />
+            Verified
+          </Badge>
+          <Badge className='h-5 min-w-5 rounded-full px-1'>8</Badge>
+          <Badge
+            className='h-5 min-w-5 rounded-full px-1'
+            variant='destructive'
+          >
+            99
+          </Badge>
+          <Badge className='h-5 min-w-5 rounded-full px-1' variant='outline'>
+            40+
+          </Badge>
+        </div>
+      </div>
+      <div className='flex  gap-4'>
+        <Toggle aria-label='Toggle italic'>
+          <Bold className='h-4 w-4' />
+        </Toggle>
+        <ToggleGroup variant='outline' type='multiple'>
+          <ToggleGroupItem value='bold' aria-label='Toggle bold'>
+            <Bold className='h-4 w-4' />
+          </ToggleGroupItem>
+          <ToggleGroupItem value='italic' aria-label='Toggle italic'>
+            <Italic className='h-4 w-4' />
+          </ToggleGroupItem>
+          <ToggleGroupItem
+            value='strikethrough'
+            aria-label='Toggle strikethrough'
+          >
+            <Underline className='h-4 w-4' />
+          </ToggleGroupItem>
+        </ToggleGroup>
       </div>
     </div>
   )
