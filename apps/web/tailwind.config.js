6d34f2a299cceefa8411585a72098b9ebbef700c
LeviLiu <liuwenyu1937@outlook.com>
2025-09-24 19:00:03 +0800

    feat: optimize font configuration

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -3,7 +3,6 @@ module.exports = {
   content: [
     './src/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
