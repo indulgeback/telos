dd08aa08d939a70cf8f40366db397ba1e3512b9c
LeviLiu <liuwenyu1937@outlook.com>
2026-06-10 16:07:36 +0800

    refactor: remove next/font/google to support offline compilation without gstatic timeouts

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,54 +1,9 @@
 import type { Metadata } from 'next'
-import {
-  Geist,
-  Geist_Mono,
-  Inter,
-  Playfair_Display,
-  JetBrains_Mono,
-  Poppins,
-} from 'next/font/google'
 import '@/styles/globals.css'
 import NextTopLoader from 'nextjs-toploader'
 import { headers } from 'next/headers'
 import appConfig from '@/appConfig'
 import { ThemeProvider } from '@/components/providers'
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-  display: 'swap',
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-  display: 'swap',
-})
-
-const inter = Inter({
-  variable: '--font-inter',
-  subsets: ['latin'],
-  display: 'swap',
-})
-
-const playfairDisplay = Playfair_Display({
-  variable: '--font-playfair',
-  subsets: ['latin'],
-  display: 'swap',
-})
-
-const jetbrainsMono = JetBrains_Mono({
-  variable: '--font-jetbrains-mono',
-  subsets: ['latin'],
-  display: 'swap',
-})
-
-const poppins = Poppins({
-  variable: '--font-poppins',
-  subsets: ['latin'],
-  weight: ['300', '400', '500', '600', '700'],
-  display: 'swap',
-})
 
 export const metadata: Metadata = {
   title: 'Telos',
@@ -76,20 +31,8 @@ export default async function RootLayout({ children }: Iprops) {
         <link rel='icon' type='image/svg+xml' href='/favicon.svg' />
         <link rel='icon' type='image/png' href='/favicon.png' />
         <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
-        <link
-          rel='preconnect'
-          href='https://fonts.googleapis.com'
-          crossOrigin='anonymous'
-        />
-        <link
-          rel='preconnect'
-          href='https://fonts.gstatic.com'
-          crossOrigin='anonymous'
-        />
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} ${playfairDisplay.variable} ${jetbrainsMono.variable} ${poppins.variable} antialiased`}
-      >
+      <body className='antialiased'>
         <NextTopLoader color='hsl(var(--foreground))' showSpinner={false} />
         <ThemeProvider
           attribute='class'
